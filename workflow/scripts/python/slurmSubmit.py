f84ae14215ce235c82d3f8e501f284521885cbae
etienne <etienne.fafard-couture@usherbrooke.ca>
2024-09-09 12:22:53 -0400

    account sbatch temp fix typo

--- workflow/scripts/python/slurmSubmit.py
+++ workflow/scripts/python/slurmSubmit.py
@@ -52,7 +52,7 @@ if job_properties["rule"] in ["genome_prediction"]:
 
 
 cmdline = "sbatch "
-cmd_line = "sbatch --acount=def-scottmic "
+cmdline = "sbatch --acount=def-scottmic "
 for param, val in job_properties['cluster'].items():
     cmdline += "--{param} {val} ".format(param=param, val=val)
 
